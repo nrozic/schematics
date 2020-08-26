9a644054dd3b357da64b9c5200a1c71ae09eff84
Nikola <nikola@trilium.io>
2020-08-26 14:53:55 +0200

    Initial working version ready for testing

---
+++ packages/code-formatting/README.md
@@ -0,0 +1,21 @@
+# Custom schematics collection
+
+Set of custom schematics to supercharge development.
+
+###Installation
+
+```shell
+npm install @nrozic/schematics
+```
+
+bug #122
+
+##Code formatting
+
+###Description
+
+Code formatting schematics provides cli command to easily configure Prettier and linters. It also adds Husky hooks and trigers linting, testing before pushing code to repository, ensuring that only properly formatted and tested code can be pushed to the repository.
+
+In order to take a full potential of this schematic, every team member should have .editorconfig support in the IDE.
+
+###Usage
--- packages/code-formatting/code-formatting/src/code-formatting/index.ts
+++ packages/code-formatting/code-formatting/src/code-formatting/index.ts
@@ -7,26 +7,58 @@ import {
     template,
     mergeWith,
     chain,
-    SchematicsException
-} from "@angular-devkit/schematics";
+    SchematicsException,
+    forEach,
+    FileEntry
+} from '@angular-devkit/schematics';
+// import { appendValueInAstArray } from "@schematics/angular/utility/json-utils";
 import {
     addPackageJsonDependency,
     NodeDependency,
     NodeDependencyType
-} from "@schematics/angular/utility/dependencies";
-import { ICodeFormatting } from "./schema";
+} from '@schematics/angular/utility/dependencies';
+import { ICodeFormatting } from './schema';
 import {
     strings,
     JsonAstObject,
     parseJsonAst,
     JsonParseMode
-} from "@angular-devkit/core";
-import { NodePackageInstallTask } from "@angular-devkit/schematics/tasks";
-import { devDependencies } from "./dependencies";
+} from '@angular-devkit/core';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import { devDependencies, Husky } from './variables';
+
+export function codeFormatting(_options: any): Rule {
+    return (tree: Tree, _context: SchematicContext) =>
+        chain([
+            addHusky(),
+            modifyTsconfig(),
+            addDependencies(),
+            editorconfig(_options)
+        ])(tree, _context);
+}
+
+export function addHusky(): Rule {
+    const filePath = '/package.json';
+
+    return (tree: Tree, _context: SchematicContext) => {
+        if (!tree.exists('/package.json')) {
+            throw new SchematicsException('File package.json does not exsist!');
+        }
+        const file = tree.read(filePath);
+        const json = JSON.parse(file!.toString());
+
+        // Add new object to package.json and assign value. It will create new object or update exsisting with new walues
+        json.husky = Husky;
+
+        console.log('JSON FILE:', json);
+        tree.overwrite('/package.json', JSON.stringify(json));
+        return tree;
+    };
+}
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-function addDevDependencies(): Rule {
+function addDependencies(): Rule {
     return (host: Tree, _context: SchematicContext) => {
         for (let pkg in devDependencies) {
             const nodeDependency: NodeDependency = _nodeDevDependencyFactory(
@@ -43,23 +75,21 @@ function addDevDependencies(): Rule {
     };
 }
 
-export function codeFormatting(_options: any): Rule {
-    return (tree: Tree, _context: SchematicContext) =>
-        chain([modifyTsconfig(), addDevDependencies(), editorconfig(_options)])(
-            tree,
-            _context
-        );
-}
-
 export function editorconfig(_options: ICodeFormatting): Rule {
     return (tree: Tree, _context: SchematicContext) => {
         if (tree) {
         }
-        const sourceTemplates = url("./files");
+        const sourceTemplates = url('./files');
         const sourceParametrizedTemplates = apply(sourceTemplates, [
             template({
                 ..._options,
                 ...strings
+            }),
+            forEach((fileEntry: FileEntry) => {
+                // Just by adding this is allows the file to be overwritten if it already exists
+                if (tree.exists(fileEntry.path))
+                    tree.overwrite('/.editorconfig', fileEntry.content);
+                return fileEntry;
             })
         ]);
 
@@ -79,34 +109,52 @@ function _nodeDevDependencyFactory(
     };
 }
 
+// function _nodeDependencyFactory(
+//     packageName: string,
+//     version: string
+// ): NodeDependency {
+//     return {
+//         type: NodeDependencyType.Default,
+//         name: packageName,
+//         version: version,
+//         overwrite: true
+//     };
+// }
+
 function modifyTsconfig(): Rule {
     return (tree: Tree, _context: SchematicContext) => {
-        if (!tree.exists("/tsconfig.json")) {
+        if (!tree.exists('/tsconfig.json')) {
             throw new SchematicsException(
-                "File tsconfig.json does not exsist!"
+                'File tsconfig.json does not exsist!'
             );
         }
 
-        let tsconfigBuffer = readTsConfig(tree, "/tsconfig.json");
+        let tsconfigBuffer = readFile(tree, '/tsconfig.json');
         // console.log("file Buffer:", tsconfigBuffer);
         if (tsconfigBuffer != null) {
             tsconfigBuffer.properties.forEach(prop => {
-                console.log("Tsconfig property:", prop);
+                prop = prop;
+                // console.log('Tsconfig property:', prop);
             });
             // tree.overwrite("/tsconfig.json", tsconfigBuffer);
         }
+        const path = `/tsconfig.json`;
+        const file = tree.read(path);
+        let json = JSON.parse(file!.toString());
+        json = json;
+        // console.log('JSON FILE:', json.compilerOptions);
         return tree;
     };
 }
 
-function readTsConfig(host: Tree, path: string): JsonAstObject {
+function readFile(host: Tree, path: string): JsonAstObject {
     const buffer = host.read(path);
     if (!buffer) {
         throw new SchematicsException(`Could not read ${path}.`);
     }
 
     const config = parseJsonAst(buffer.toString(), JsonParseMode.Loose);
-    if (config.kind !== "object") {
+    if (config.kind !== 'object') {
         throw new SchematicsException(
             `Invalid ${path}. Was expecting an object.`
         );
