f6245a894e7371c7d179c03fbdefe904fea6852e
Nikola <nikola@trilium.io>
2020-10-25 02:38:47 +0100

    Error fix

--- packages/code-formatting/src/code-formatting/index.ts
+++ packages/code-formatting/src/code-formatting/index.ts
@@ -36,9 +36,9 @@ export function codeFormatting(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) =>
     chain([
       addHusky(),
-      modifyTsconfig(),
       addDependencies(),
       editorconfig(_options),
+      //   modifyTsconfig(),
     ])(tree, _context);
 }
 
@@ -140,38 +140,39 @@ function _nodeDevDependencyFactory(
 //     };
 // }
 
-function modifyTsconfig(): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    const path = '/tsconfig.json';
+// function modifyTsconfig(): Rule {
+//   return (tree: Tree, _context: SchematicContext) => {
+//     const path = '/tsconfig.json';
 
-    if (!tree.exists(path)) {
-      throw new SchematicsException('File tsconfig.json does not exsist!');
-    }
+//     if (!tree.exists(path)) {
+//       throw new SchematicsException('File tsconfig.json does not exsist!');
+//     }
 
-    const file = tree.read(path);
-    let json = JSON.parse(file!.toString());
-    json.paths = {
-      'core-js/es7/reflect': [
-        'node_modules/core-js/proposals/reflect-metadata',
-      ],
-      'core-js/es6/*': ['node_modules/core-js/es/*'],
-      '@assets/*': ['src/assets/*'],
-      '@env/*': ['src/environments/*'],
-      '@ui/*': ['src/app/common/ui-components/*'],
-      '@constants/*': ['src/app/common/constants/*'],
-      '@models/*': ['src/app/common/models/*'],
-      '@guards/*': ['src/app/common/guards/*'],
-      '@animations/*': ['src/app/common/animations/*'],
-      '@layouts/*': ['src/app/common/layouts*'],
-      '@common/*': ['src/app/common/*'],
-      '@app/*': ['src/app/*'],
-      '@src/*': ['src/*'],
-      '@e2e/*': ['e2e/*'],
-    };
-    console.log('JSON FILE:', json.compilerOptions);
-    return tree;
-  };
-}
+//     const file = tree.read(path);
+//     let json = JSON.parse(file!.toString());
+//     json.paths = {
+//       'core-js/es7/reflect': [
+//         'node_modules/core-js/proposals/reflect-metadata',
+//       ],
+//       'core-js/es6/*': ['node_modules/core-js/es/*'],
+//       '@assets/*': ['src/assets/*'],
+//       '@env/*': ['src/environments/*'],
+//       '@ui/*': ['src/app/common/ui-components/*'],
+//       '@constants/*': ['src/app/common/constants/*'],
+//       '@models/*': ['src/app/common/models/*'],
+//       '@guards/*': ['src/app/common/guards/*'],
+//       '@animations/*': ['src/app/common/animations/*'],
+//       '@layouts/*': ['src/app/common/layouts*'],
+//       '@common/*': ['src/app/common/*'],
+//       '@app/*': ['src/app/*'],
+//       '@src/*': ['src/*'],
+//       '@e2e/*': ['e2e/*'],
+//     };
+//     console.log('JSON FILE:', json.compilerOptions);
+//     tree.overwrite(path, JSON.stringify(json));
+//     return tree;
+//   };
+// }
 
 // function readFile(host: Tree, path: string): JsonAstObject {
 //     const buffer = host.read(path);
