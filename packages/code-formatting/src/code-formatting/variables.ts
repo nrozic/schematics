517347ce9ec21912debf7602fd223c46a53f204b
Nikola <nikola@trilium.io>
2020-10-25 19:45:28 +0100

    All dependencies will use latest version

--- packages/code-formatting/src/code-formatting/variables.ts
+++ packages/code-formatting/src/code-formatting/variables.ts
@@ -1,15 +1,15 @@
 export const devDependencies: { [key: string]: string } = {
-  husky: '@latest',
-  prettier: '@latest',
-  'prettier-stylelint': '@latest',
-  'pretty-quick': '@latest',
-  stylelint: '@latest',
-  'stylelint-config-recommended': '@latest',
-  tslint: '@latest',
-  'tslint-config-prettier': '@latest',
-  'stylelint-order': '@latest',
-  'stylelint-prettier': '@latest',
-  'tslint-eslint-rules': '@latest',
+  husky: '*',
+  prettier: '*',
+  'prettier-stylelint': '*',
+  'pretty-quick': '*',
+  stylelint: '*',
+  'stylelint-config-recommended': '*',
+  tslint: '*',
+  'tslint-config-prettier': '*',
+  'stylelint-order': '*',
+  'stylelint-prettier': '*',
+  'tslint-eslint-rules': '*',
 };
 
 export const Husky: { [key: string]: object } = {
