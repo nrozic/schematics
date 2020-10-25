546b99c35f72d890ee20af9e0f51de56e39e2350
Nikola <nikola@trilium.io>
2020-10-25 02:25:44 +0100

    Testing fix

--- packages/code-formatting/src/code-formatting/index.ts
+++ packages/code-formatting/src/code-formatting/index.ts
@@ -1,28 +1,28 @@
 import {
-    Rule,
-    SchematicContext,
-    Tree,
-    url,
-    apply,
-    template,
-    mergeWith,
-    chain,
-    SchematicsException,
-    forEach,
-    FileEntry
+  Rule,
+  SchematicContext,
+  Tree,
+  url,
+  apply,
+  template,
+  mergeWith,
+  chain,
+  SchematicsException,
+  forEach,
+  FileEntry,
 } from '@angular-devkit/schematics';
 // import { appendValueInAstArray } from "@schematics/angular/utility/json-utils";
 import {
-    addPackageJsonDependency,
-    NodeDependency,
-    NodeDependencyType
+  addPackageJsonDependency,
+  NodeDependency,
+  NodeDependencyType,
 } from '@schematics/angular/utility/dependencies';
 import { ICodeFormatting } from './schema';
 import {
-    strings
-    // JsonAstObject,
-    // parseJsonAst,
-    // JsonParseMode
+  strings,
+  // JsonAstObject,
+  // parseJsonAst,
+  // JsonParseMode
 } from '@angular-devkit/core';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 import { devDependencies, Husky } from './variables';
@@ -33,54 +33,54 @@ import { devDependencies, Husky } from './variables';
  * @param _options Input options
  */
 export function codeFormatting(_options: any): Rule {
-    return (tree: Tree, _context: SchematicContext) =>
-        chain([
-            addHusky(),
-            modifyTsconfig(),
-            addDependencies(),
-            editorconfig(_options)
-        ])(tree, _context);
+  return (tree: Tree, _context: SchematicContext) =>
+    chain([
+      addHusky(),
+      modifyTsconfig(),
+      addDependencies(),
+      editorconfig(_options),
+    ])(tree, _context);
 }
 
 /**
  * This method will add preconfigured husky hooks to package.json.
  */
 export function addHusky(): Rule {
-    const filePath = '/package.json';
+  const filePath = '/package.json';
 
-    return (tree: Tree, _context: SchematicContext) => {
-        if (!tree.exists('/package.json')) {
-            throw new SchematicsException('File package.json does not exsist!');
-        }
-        const file = tree.read(filePath);
-        const json = JSON.parse(file!.toString());
+  return (tree: Tree, _context: SchematicContext) => {
+    if (!tree.exists('/package.json')) {
+      throw new SchematicsException('File package.json does not exsist!');
+    }
+    const file = tree.read(filePath);
+    const json = JSON.parse(file!.toString());
 
-        json.husky = Husky;
+    json.husky = Husky;
 
-        console.log('JSON FILE:', json);
-        tree.overwrite('/package.json', JSON.stringify(json));
-        return tree;
-    };
+    console.log('JSON FILE:', json);
+    tree.overwrite('/package.json', JSON.stringify(json));
+    return tree;
+  };
 }
 
 /**
  * It will add dev dependencies required for code formatting to package.json and install them for you.
  */
 function addDependencies(): Rule {
-    return (host: Tree, _context: SchematicContext) => {
-        for (let pkg in devDependencies) {
-            const nodeDependency: NodeDependency = _nodeDevDependencyFactory(
-                pkg,
-                devDependencies[pkg]
-            );
+  return (host: Tree, _context: SchematicContext) => {
+    for (let pkg in devDependencies) {
+      const nodeDependency: NodeDependency = _nodeDevDependencyFactory(
+        pkg,
+        devDependencies[pkg]
+      );
 
-            addPackageJsonDependency(host, nodeDependency);
-        }
+      addPackageJsonDependency(host, nodeDependency);
+    }
 
-        // console.log("host:", host, "kontekst:", _context);
+    // console.log("host:", host, "kontekst:", _context);
 
-        _context.addTask(new NodePackageInstallTask());
-    };
+    _context.addTask(new NodePackageInstallTask());
+  };
 }
 
 /**
@@ -89,25 +89,25 @@ function addDependencies(): Rule {
  * @param _options Input params
  */
 export function editorconfig(_options: ICodeFormatting): Rule {
-    return (tree: Tree, _context: SchematicContext) => {
-        if (tree) {
-        }
-        const sourceTemplates = url('./files');
-        const sourceParametrizedTemplates = apply(sourceTemplates, [
-            template({
-                ..._options,
-                ...strings
-            }),
-            forEach((fileEntry: FileEntry) => {
-                // Just by adding this is allows the file to be overwritten if it already exists
-                if (tree.exists(fileEntry.path))
-                    tree.overwrite('/.editorconfig', fileEntry.content);
-                return fileEntry;
-            })
-        ]);
+  return (tree: Tree, _context: SchematicContext) => {
+    if (tree) {
+    }
+    const sourceTemplates = url('./files');
+    const sourceParametrizedTemplates = apply(sourceTemplates, [
+      template({
+        ..._options,
+        ...strings,
+      }),
+      forEach((fileEntry: FileEntry) => {
+        // Just by adding this is allows the file to be overwritten if it already exists
+        if (tree.exists(fileEntry.path))
+          tree.overwrite('/.editorconfig', fileEntry.content);
+        return fileEntry;
+      }),
+    ]);
 
-        return mergeWith(sourceParametrizedTemplates);
-    };
+    return mergeWith(sourceParametrizedTemplates);
+  };
 }
 
 /**
@@ -117,15 +117,15 @@ export function editorconfig(_options: ICodeFormatting): Rule {
  * @param version target package version
  */
 function _nodeDevDependencyFactory(
-    packageName: string,
-    version: string
+  packageName: string,
+  version: string
 ): NodeDependency {
-    return {
-        type: NodeDependencyType.Dev,
-        name: packageName,
-        version: version,
-        overwrite: true
-    };
+  return {
+    type: NodeDependencyType.Dev,
+    name: packageName,
+    version: version,
+    overwrite: true,
+  };
 }
 
 // function _nodeDependencyFactory(
@@ -141,21 +141,18 @@ function _nodeDevDependencyFactory(
 // }
 
 function modifyTsconfig(): Rule {
-    return (tree: Tree, _context: SchematicContext) => {
-        // const path = '/tsconfig.json'
-        if (!tree.exists('/tsconfig.json')) {
-            throw new SchematicsException(
-                'File tsconfig.json does not exsist!'
-            );
-        }
+  return (tree: Tree, _context: SchematicContext) => {
+    const path = '/tsconfig.json';
+    if (!tree.exists(path)) {
+      throw new SchematicsException('File tsconfig.json does not exsist!');
+    }
 
-        const tsconfigpath = `/tsconfig.json`;
-        const file = tree.read(tsconfigpath);
-        let json = JSON.parse(file!.toString());
-        json = json;
-        // console.log('JSON FILE:', json.compilerOptions);
-        return tree;
-    };
+    const file = tree.read(path);
+    let json = JSON.parse(file!.toString());
+    json = json;
+    console.log('JSON FILE:', json.compilerOptions);
+    return tree;
+  };
 }
 
 // function readFile(host: Tree, path: string): JsonAstObject {
